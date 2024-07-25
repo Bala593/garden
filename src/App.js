c9288b7444c1bc20ee6f138fa7596648e9dbcf15
Bala593 <balapraveen0509@gmail.com>
2024-07-25 22:35:38 +0530

    project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SwapComponent from './Components/SwapComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <SwapComponent />
+    </div>
+  );
+}
+
+export default App;
